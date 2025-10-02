1005263bd75df2304ce777b95931b8b73b5208bd
Gromakov Daniil <danila.gromakov@gmail.com>
2025-10-03 00:35:54 +0300

    feat(): тесты

--- database.py
+++ database.py
@@ -1,20 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
 
+DATABASE_URL = settings.database_url
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
 Base = declarative_base()
-
-
-def get_engine(database_url: str = None):
-    url = database_url or settings.database_url
-    return create_async_engine(url, future=True)
-
-
-def get_sessionmaker(engine=None):
-    if engine is None:
-        engine = get_engine()
-    return sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-
-engine = get_engine()
-async_session = get_sessionmaker(engine)
